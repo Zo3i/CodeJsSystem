55b2b791ef62d7384f171c7eb3a4fb513c49f85d
Zo3i <30296801+Zo3i@users.noreply.github.com>
2019-03-01 10:10:04 +0800

    fix

--- web/src/main/java/com/jeesite/modules/config/Application.java
+++ web/src/main/java/com/jeesite/modules/config/Application.java
@@ -19,15 +19,12 @@ import com.jeesite.common.io.PropertiesUtils;
 public class Application extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Application.class);
-		app.setDefaultProperties(PropertiesUtils.getInstance().getProperties());
-		app.run(args);
+		SpringApplication.run(Application.class, args);
 	}
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		this.setRegisterErrorPageFilter(false); // 错误页面有容器来处理，而不是SpringBoot
-		builder.properties(PropertiesUtils.getInstance().getProperties());
 		return builder.sources(Application.class);
 	}
 	
