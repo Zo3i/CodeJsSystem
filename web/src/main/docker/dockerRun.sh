d8c51e4ba6207b17a4c14368d9b9a3a3478a8d92
Zo3i <30296801+Zo3i@users.noreply.github.com>
2019-03-04 13:22:45 +0800

    fix api

--- web/src/main/docker/dockerRun.sh
+++ web/src/main/docker/dockerRun.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+#rm local file
+rm -rf CodeJs*
+
 #down file
 git clone https://github.com/Zo3i/CodeJsFront.git
 git clone https://github.com/Zo3i/CodeJsSystem.git
 
-cp ./CodeJsFront/dist/* ./CodeJsSystem/web/src/main/docker/nginx/dist/
-
+mv ./CodeJsFront/dist/ ./CodeJsSystem/web/src/main/docker/nginx/
 #enter path
 cd ./CodeJsSystem/web/src/main/docker
 chmod -R 755 ./*
@@ -14,7 +16,7 @@ chmod -R 755 ./*
 docker-compose down
 
 read -p "first creat input(1):" first
-if ["$first" -eq 1];
+if [ "$first" -eq 1 ];
 then
  cd ./mysql
  docker build -t js/mysql .
@@ -22,4 +24,4 @@ then
 fi
 
 docker-compose build
-docker-compose up
+docker-compose up -d
